327795bd7fd527f52adb43d09ad76716c3604b47
zeryx <1892175+zeryx@users.noreply.github.com>
2023-09-19 12:15:28 -0400

    removed unnecessary time.sleep
    Signed-off-by: zeryx <1892175+zeryx@users.noreply.github.com>

--- workshop_2_client_server_comm/workflows/client_server.py
+++ workshop_2_client_server_comm/workflows/client_server.py
@@ -2,7 +2,7 @@
 
 import typing
 from flytekit import task, workflow, PodTemplate
-from time import time, sleep
+from time import time
 import requests
 from kubernetes.client import V1PodSpec, V1Container, V1ResourceRequirements
 
@@ -43,8 +43,6 @@ pod = PodTemplate(
 @task(pod_template=pod, container_image=client_img)
 def client_function(name: str) -> dict:
     try:
-        sleep(1)
-        print("Slept for 1 second, waiting for server to deploy")
         t0 = time()
         response = requests.post(interact_url, json={"name": name})
         t1 = time()
