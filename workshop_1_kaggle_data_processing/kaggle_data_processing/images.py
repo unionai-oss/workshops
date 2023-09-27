41324bf583da1d2ca234746f36a4d593972e679b
zeryx <1892175+zeryx@users.noreply.github.com>
2023-09-27 15:19:33 -0400

    corrected trailing /
    Signed-off-by: zeryx <1892175+zeryx@users.noreply.github.com>

--- workshop_1_kaggle_data_processing/kaggle_data_processing/images.py
+++ workshop_1_kaggle_data_processing/kaggle_data_processing/images.py
@@ -1,15 +1,15 @@
 from flytekit import ImageSpec
 
 # Your image registry url, if using ghcr.io, this will be "ghcr.io/<your-username>"
-registry_prefix = "ghcr.io/<your-username>"
+registry_prefix = "ghcr.io/<your-github-organization>"
 
 
 # This image is used by the ContainerTask obtaining the dataset with the Kaggle API
-get_dataset_package_name_tag = "dedupe:latest"
+get_dataset_package_name_tag = "get_dataset:latest"
 
 
 # This image is used by the task deduplicating the dataset, and is utilized in the ImageSpec below
-dedupe_image_package_name = "default"
+dedupe_image_package_name = "dedupe"
 
 dedupe_image = ImageSpec(
     name=dedupe_image_package_name,
@@ -17,6 +17,5 @@ dedupe_image = ImageSpec(
     registry=registry_prefix,
     packages=["flytekit", "pandas==1.5.3"],
     apt_packages=["git"],
-
 )
 
