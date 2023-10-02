93e70c97e7cbccffb6c9b044ac92238087af933b
Peeter Piegaze <peeter@union.ai>
2023-10-02 15:47:09 +0200

    Move user-controlled variables into config.py

--- workshop_1_kaggle_data_processing/kaggle_data_processing/tests/test_workflows.py
+++ workshop_1_kaggle_data_processing/kaggle_data_processing/tests/test_workflows.py
@@ -11,7 +11,7 @@ from pathlib import Path
 from .cases import WorkflowCase, WORKFLOW_CASES
 from flytekit.remote import FlyteRemote
 from flytekit.configuration import Config
-from ..names import target_project, target_domain
+from ..config import target_project, target_domain
 
 
 class TestWorkflow:
--- workshop_1_kaggle_data_processing/kaggle_data_processing/workflows/deduplication.py
+++ workshop_1_kaggle_data_processing/kaggle_data_processing/workflows/deduplication.py
@@ -2,14 +2,12 @@ import flytekit
 from flytekit import task, workflow, ContainerTask, kwtypes, Resources, TaskMetadata
 from flytekit.types.file import FlyteFile
 from flytekit import Secret
-from ..images import dedupe_image, registry_prefix, get_dataset_package_name_tag
-
-SECRET_GROUP = "arn:aws:secretsmanager:<your-aws-region>:<your-aws-account>:"
-SECRET_KEY = "<your-secret-org>/<your-secret-name"
+from ..config import registry_prefix, kaggle_task_image_name_and_tag, SECRET_KEY, SECRET_GROUP
+from ..images import dedupe_task_image_spec
 
 get_dataset = ContainerTask(
     name="get_dataset",
-    image=f"{registry_prefix}/{get_dataset_package_name_tag}",
+    image=f"{registry_prefix}/{kaggle_task_image_name_and_tag}",
     input_data_dir="/var/inputs",
     output_data_dir="/var/outputs",
     inputs=kwtypes(dataset_name=str, file_name=str, kaggle_config=str),
@@ -24,12 +22,13 @@ get_dataset = ContainerTask(
     metadata=TaskMetadata(retries=5)
 )
 
+
 @task(
     secret_requests=[
         Secret(
-        group=SECRET_GROUP,
-        key=SECRET_KEY,
-        mount_requirement=Secret.MountType.FILE)
+            group=SECRET_GROUP,
+            key=SECRET_KEY,
+            mount_requirement=Secret.MountType.FILE)
     ],
 )
 def get_credentials() -> str:
@@ -39,11 +38,12 @@ def get_credentials() -> str:
 
 # As pandas may not be installed locally, we only import it if we are running in a container.
 # We still validate the workflow, but we don't fully trace the IO.
-# We do that with the `.is_container` property of imagespec objects.
-if dedupe_image.is_container:
+# We do that by testing for the `.is_container` property o=n the ImageSpec object.
+if dedupe_task_image_spec.is_container:
     import pandas as pd
-    @task(container_image=dedupe_image, requests=Resources(cpu="2", mem="10Gi", ephemeral_storage="10Gi"))
-    def deduplicate_dataset(dataset: FlyteFile) -> pd.DataFrame:
+
+    @task(container_image=dedupe_task_image_spec, requests=Resources(cpu="2", mem="10Gi", ephemeral_storage="10Gi"))
+    def dedupe_task(dataset: FlyteFile) -> pd.DataFrame:
         print("getting dataset as csv")
 
         df = pd.read_csv(dataset, on_bad_lines='skip')
@@ -55,5 +55,5 @@ if dedupe_image.is_container:
     def deduplication_wf(dataset_name: str, file_name: str) -> pd.DataFrame:
         config = get_credentials()
         dataset: FlyteFile = get_dataset(dataset_name=dataset_name, file_name=file_name, kaggle_config=config)
-        deduped_data = deduplicate_dataset(dataset=dataset)
+        deduped_data = dedupe_task(dataset=dataset)
         return deduped_data
